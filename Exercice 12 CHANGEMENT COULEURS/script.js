0b1a50ff05847fab3633854dd83f25032c6350f3
jasondev12 <maesjasonpro@gmail.com>
2020-04-22 09:01:27 +0200

    maj change couleur création html css et début js

--- Exercice 12 CHANGEMENT COULEURS/script.js
+++ Exercice 12 CHANGEMENT COULEURS/script.js
@@ -1,1 +1,6 @@
-
+// recuperation des éléments du html
+//création des variables
+//récupération du body
+var body = document.getElementById("body");
+//récupération des inputs
+var inputs = document.querySelectorAll("input");
