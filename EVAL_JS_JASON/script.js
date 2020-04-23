8f3da2ab29945823645c21096ecff1ba64362bb8
jasondev12 <maesjasonpro@gmail.com>
2020-04-23 11:04:53 +0200

    MAJ3 JS EVAL

--- EVAL_JS_JASON/script.js
+++ EVAL_JS_JASON/script.js
@@ -3,8 +3,15 @@ var capital ; // capital emprunte
 var duree ;  // duree d'emprunt
 var taux ;  // taux mensuel
 var mensualite ;  // mensualite de remboursement
+
+let input = document.querySelectorAll("#calcul");
+let error = document.querySelectorAll('p');
+
+for (let i = 0; i < input.length; i++) {
+    error[i].style.display = "none";
+  }
+
 // Etape 1 - Verification utilisateur entre un nmbre positif
- 
 function verifier(nom, valeur){
     i = new Number(0);
     if (isNaN(valeur) == true){
