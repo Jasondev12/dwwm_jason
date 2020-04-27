0c1db4a2a99b4b2a014278be8f58f334e05c0e54
jasondev12 <maesjasonpro@gmail.com>
2020-04-27 08:38:43 +0200

    maj api météo

--- API météo/post.js
+++ API météo/post.js
@@ -3,18 +3,21 @@
 let villeChoisie = "bergues";
 recevoirTemperature(villeChoisie);
 
-let changerDeVille = document.querySelector('#changer');
-changerDeVille.addEventListener('click', () => {
-    villeChoisie = prompt('Quelle ville souhaitez-vous voir ?');
+let changerDeVille = document.querySelector("#changer");
+changerDeVille.addEventListener("click", () => {
+    villeChoisie = prompt("Quelle ville souhaitez-vous voir ?");
     recevoirTemperature(villeChoisie);
 });
 
 function recevoirTemperature(ville) {
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=229a7f1ca981d5bf71a3e305c2af64ea&units=metric';
+    const url =
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        ville +
+        "&appid=229a7f1ca981d5bf71a3e305c2af64ea&units=metric";
 
     let requete = new XMLHttpRequest(); // Nous créons un objet qui nous permettra de faire des requêtes
-    requete.open('GET', url); // Nous récupérons juste des données
-    requete.responseType = 'json'; // Nous attendons du JSON
+    requete.open("GET", url); // Nous récupérons juste des données
+    requete.responseType = "json"; // Nous attendons du JSON
     requete.send(); // Nous envoyons notre requête
 
     // Dès qu'on reçoit une réponse, cette fonction est executée
@@ -25,18 +28,36 @@ function recevoirTemperature(ville) {
                 console.log(reponse);
                 let temperature = reponse.main.temp;
                 let ville = reponse.name;
-                let icon = reponse.weather[0].icon;
-                let iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
-                console.log(temperature);
-                console.log(ville);
-                console.log(icon);
-                console.log(iconurl);
-                document.querySelector('#temperature_label').textContent = Math.round(temperature);
-                document.querySelector('#ville').textContent = ville;
-                document.querySelector("#iconurl").innerHTML = '<img src="' + iconurl + '" />';
+                let icon = reponse.weather[0].icon; // nous avons le code de l'icone ex: 02D
+                let iconurl = "http://openweathermap.org/img/w/" + icon + ".png"; // ce code au dessus est mis dans le lien
+                let vitesseVent = reponse.wind.speed;
+                //console.log(temperature);
+                //console.log(ville);
+                //console.log(icon);
+                //console.log(iconurl);
+
+                console.log(vitesseVent);
+                document.querySelector("#temperature_label").textContent = Math.round(
+                    temperature
+                );
+                document.querySelector("#ville").textContent = ville;
+                document.querySelector("#iconurl").innerHTML =
+                    '<img src="' + iconurl + '" />';
+                document.querySelector("#vitesseVent").innerHTML =
+                    "<div class=vitesseVent>Vitesse du vent : " +
+                    vitesseVent +
+                    " Km/h</div>";
             } else {
-                alert('Un problème est intervenu, merci de revenir plus tard.');
+                alert("Un problème est intervenu, merci de revenir plus tard.");
             }
         }
-    }
+    };
 }
+
+function heure() {
+    var ladate = new Date();
+    var n = ladate.toLocaleTimeString();
+    console.log(n);
+    document.querySelector("#heure").innerHTML = n;
+}
+setInterval("heure()", 1000);
