38f6c78e0c75345b9b51f30c9a80f8cee2a9c024
jasondev12 <maesjasonpro@gmail.com>
2020-05-11 17:27:32 +0200

    Création plugin enregistrement commentaires en bdd

--- WORDPRESS/wordpress 2/wp-content/plugins/HelloWorld/helloworld.php
+++ WORDPRESS/wordpress 2/wp-content/plugins/HelloWorld/helloworld.php
@@ -1,14 +1,18 @@
 <?php
 /*
 Plugin Name: Hello World
-Description: ecrit Hello World
-Author: Jason
+Description: Plugin qui enregistre les commentaires en bdd
+Author: Martine
 Version: 1.0
-*/
-class HelloWorld_Plugin{
-public function __construct(){
-include_once plugin_dir_path(__FILE__).'/helloclass.php';
-new HelloClass();
-}
+ */
+class HelloWorld_Plugin
+{
+    public function __construct()
+    {
+        include_once plugin_dir_path(__FILE__) . '/helloClass.php';
+        register_activation_hook(__FILE__, array('helloclass', 'install'));
+        new HelloClass();
+        
+    }
 }
 new HelloWorld_Plugin();
